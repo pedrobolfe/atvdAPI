8ab73517d520d73573f386115ec0d7cb64885004
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-08-14 17:25:49 -0300

    Update script.js

--- CloudConverter/script.js
+++ CloudConverter/script.js
@@ -1,1 +1,9 @@
-aa
+var fs = require('fs'),
+  cloudconvert = new (require('cloudconvert'))('');
+
+fs.createReadStream('valorinput.mp4')
+.pipe(cloudconvert.convert({
+    "inputformat": "mp4,
+    "outputformat": "mp3",
+    "input": "upload"
+})).pipe(fs.createWriteStream("outputfile.mp3"));
